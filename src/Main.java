90b3578803671dbb6e197671da386fae35497fb9
DAM <roberto.toquero19@gmail.com>
2023-10-09 14:13:00 +0200

    ..

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        Cliente cliente1= new Cliente("Cliente 1", new int[]{2,2,1,5,2,3});
+        Cliente cliente2= new Cliente("Cliente 2", new int[]{1,3,5,1,1});
+        Cliente cliente3= new Cliente("Cliente3",  new int[]{2,5,3,4,7});
 
-        }
+        long initialTime=System.currentTimeMillis();
+        //System.out.println(initialTime);
+
+        CajeraThread Cajera1=new CajeraThread("Cajera1",cliente1,initialTime);
+        CajeraThread Cajera2=new CajeraThread("Cajera2",cliente2,initialTime);
+        CajeraThread Cajera3=new CajeraThread("Cajera3",cliente3,initialTime);
+
+        Cajera1.start();
+        Cajera2.start();
+        Cajera3.start();
+        System.out.println(Cajera1);
+        System.out.println(Cajera2);
+        System.out.println(Cajera3);
+
+    }
  }
